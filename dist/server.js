db4381d5b0f1bd9d8a2f0228f1ba4b339cc6e08d
Julio <juliorosseti@gmail.com>
2021-10-19 12:10:09 -0300

    Alterações no dotenv p/ heroku

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
 const app_1 = require("./app");
 app_1.app.listen(process.env.PORT);
